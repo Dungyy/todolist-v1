d8142ee61cbea895885da5dab3f133e348e393d1
Dungyy <erickmunoz13@gmail.com>
2021-12-03 12:26:53 -0600

    added css styles

--- app.js
+++ app.js
@@ -6,37 +6,48 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-var items = ["Buy food", "Cook Food", "Eat Food"];
+let items = ["Buy food", "cook food", "eat food"];
+
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
     let today = new Date();
 
-const options = {
+let options = {
     weekday: "long",
     day: "numeric",
     month: "long" 
 };
 
-const day = today.toDateString("en-US", options);
+let day = today.toDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    const item = req.body.newItem;
 
     items.push(item);
-
+    console.log(req.body);
     res.redirect("/");
 });
 
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
 
 app.listen(PORT, function(){
     console.log(`Server running on port ${PORT}`);
